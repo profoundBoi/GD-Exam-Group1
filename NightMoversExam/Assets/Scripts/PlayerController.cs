ecadf7399501c558361ad651af18ba4f53db1c15
lxrdflame <tyrique.s.r@gmail.com>
2026-05-22 17:07:56 +0200

    working on Split Screen

--- NightMoversExam/Assets/Scripts/PlayerController.cs
+++ NightMoversExam/Assets/Scripts/PlayerController.cs
@@ -20,9 +20,6 @@ public class PlayerController3D : MonoBehaviour
     [Header("Movement")]
     public float speed = 5f;
     public float jumpForce = 5f;
-    public float SpeedMultiplier;
-    private float baseSpeed;
-    private float RunSpeed;
 
     [Header("Look")]
     public float lookSensitivity = 120f;
@@ -36,59 +33,22 @@ public class PlayerController3D : MonoBehaviour
 
     private GameObject InteractableObject;
     public LayerMask Interact;
+
     [SerializeField]
     private int Interactrange;
     private GameObject CurrentInteractableObject;
     public Transform HoldPosition;
     public Transform MidlePoint;
+
     [SerializeField]
     public GameObject heldObject;
     private ObjectweightManager currentHeavyObject;
     public Transform MedianPoint;
 
-    [SerializeField] private bool isChargingWeapon;
-    [SerializeField] private float attackPower;
-    [SerializeField] private GameObject heldWeapon;
-    [SerializeField] private int AimDistance;
-    private bool isShooting;
-
-    public GameObject SniperScopeUi;
-
-    [Header("UI controls")]
-    [SerializeField] private GameObject PausePanel;
-    [SerializeField] private Canvas PauseCanvas, InventoryCanvas;
-    [SerializeField] private GameObject InventoryPanel;
-
-    private PlayerInputManager playerInputManager;
-    private GameObject playerInputmNagerHolder;
-    [SerializeField] private MultiplayerEventSystem eventSystem;
-    [SerializeField] private GameObject PauseFirstSelect, InventoryFirstSelect;
-
     [Header("Animations")]
     [SerializeField] private Animator playerAnimations;
     private bool isJumping;
     [SerializeField] private List<string> AnimationBools;
-
-    [Header("Enemy Stats")]
-    public GameObject OtherPlayer;
-    private PlayerController3D otherPlayersScript;
-
-    [Header("Slide Stats")]
-    public float forceAmount = 20f;
-    public float forceDuration = 1f;
-    private bool isRunning;
-    private bool isSliding;
-
-    [Header("Audio")]
-    [SerializeField] private AudioSource audioSource;
-    [SerializeField] private AudioClip shootSound;
-
-    [Header("Map Settings")]
-    [SerializeField] private Transform PlayerMiniMap;
-    [SerializeField] private Transform OpenMapCamera;
-    [SerializeField] private Vector3 OpenMap, ClosedMap;
-    [SerializeField] private Vector3 OpenMapPosition, ClosedMapPosition;
-    [SerializeField] private Vector3 OpenMapCameraPosition, ClosedMapCameraPosition;
 
     [Header("Carry Object Settings")]
     [SerializeField] private ObjectweightManager ObjectScript;
@@ -103,8 +63,6 @@ public class PlayerController3D : MonoBehaviour
     void Start()
     {
         animator = GetComponent<Animator>();
-        baseSpeed = speed;
-        RunSpeed = speed * SpeedMultiplier;
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -122,68 +80,6 @@ public class PlayerController3D : MonoBehaviour
     {
         if (context.performed && IsGrounded())
             rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
-    }
-
-    public void OnRun(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            isRunning = true;
-            speed = RunSpeed;
-        }
-        else if (context.canceled)
-        {
-            isRunning = false;
-            speed = baseSpeed;
-        }
-    }
-
-    public void OnMapOpen(InputAction.CallbackContext context)
-    {
-        if (context.performed)
-        {
-            PlayerMiniMap.localScale = OpenMap;
-            PlayerMiniMap.GetComponent<RectTransform>().position = OpenMapPosition;
-            OpenMapCamera.localPosition = OpenMapCameraPosition;
-        }
-        else if (context.canceled)
-        {
-            PlayerMiniMap.localScale = ClosedMap;
-            PlayerMiniMap.GetComponent<RectTransform>().position = ClosedMapPosition;
-            OpenMapCamera.localPosition = ClosedMapCameraPosition;
-        }
-    }
-
-    public void OnOpenInventorysystem(InputAction.CallbackContext context)
-    {
-        if (InventoryPanel.activeSelf)
-        {
-            InventoryPanel.SetActive(false);
-            speed = baseSpeed;
-        }
-        else
-        {
-            eventSystem.firstSelectedGameObject = InventoryFirstSelect;
-            eventSystem.playerRoot = InventoryCanvas.gameObject;
-            InventoryPanel.SetActive(true);
-            speed = 0;
-        }
-    }
-
-    public void PauseandPlay(InputAction.CallbackContext context)
-    {
-        if (PausePanel.activeSelf)
-        {
-            PausePanel.SetActive(false);
-            Time.timeScale = 1f;
-        }
-        else
-        {
-            PausePanel.SetActive(true);
-            Time.timeScale = 0f;
-            eventSystem.playerRoot = PauseCanvas.gameObject;
-            eventSystem.firstSelectedGameObject = PauseFirstSelect;
-        }
     }
 
     public void OnInteract(InputAction.CallbackContext context)
