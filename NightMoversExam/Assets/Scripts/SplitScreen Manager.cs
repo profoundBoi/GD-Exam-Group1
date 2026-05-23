41c6df25540389c202521538fa9236edb108c656
lxrdflame <tyrique.s.r@gmail.com>
2026-05-23 13:39:42 +0200

    Camera

--- NightMoversExam/Assets/Scripts/SplitScreen Manager.cs
+++ NightMoversExam/Assets/Scripts/SplitScreen Manager.cs
@@ -1,37 +1,16 @@
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class SplitScreenManager : MonoBehaviour
 {
-    private PlayerInputManager playerInputManager;
+    [SerializeField] private Camera player1Camera;
+    [SerializeField] private Camera player2Camera;
 
-    void Awake()
+    void Start()
     {
-        playerInputManager = GetComponent<PlayerInputManager>();
-        playerInputManager.onPlayerJoined += OnPlayerJoined;
-    }
+        // Top half
+        player1Camera.rect = new Rect(0f, 0.5f, 1f, 0.5f);
 
-    void OnDestroy()
-    {
-        playerInputManager.onPlayerJoined -= OnPlayerJoined;
-    }
-
-    void OnPlayerJoined(PlayerInput player)
-    {
-        Camera cam = player.GetComponentInChildren<Camera>();
-        if (cam == null) return;
-
-        int playerIndex = player.playerIndex;
-
-        if (playerIndex == 0)
-        {
-            // Top half
-            cam.rect = new Rect(0f, 0.5f, 1f, 0.5f);
-        }
-        else if (playerIndex == 1)
-        {
-            // Bottom half
-            cam.rect = new Rect(0f, 0f, 1f, 0.5f);
-        }
+        // Bottom half
+        player2Camera.rect = new Rect(0f, 0f, 1f, 0.5f);
     }
 }
