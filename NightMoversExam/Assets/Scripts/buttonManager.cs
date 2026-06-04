1e5376fdeb41edcdfda0f1b036661d7bb40940d5
lxrdflame <tyrique.s.r@gmail.com>
2026-06-04 23:20:29 +0200

    UI Navigation

--- NightMoversExam/Assets/Scripts/buttonManager.cs
+++ NightMoversExam/Assets/Scripts/buttonManager.cs
@@ -1,10 +1,21 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class buttonManager : MonoBehaviour
 {
     [SerializeField]
     private string neighbourhoodScene;
+
+    [SerializeField]
+    private GameObject PausePanel, ControlsPanel;
+
+    [SerializeField]
+    private EventSystem eventSystem;
+
+    [SerializeField]
+    private GameObject currentSelectbutton;
 
     public void StartGame()
     {
@@ -18,6 +29,30 @@ public class buttonManager : MonoBehaviour
 
     public void OpenControls()
     {
+        ControlsPanel.SetActive(true);
+    }
 
+    public void ResumeGame()
+    {
+        Time.timeScale = 1.0f;
+        PausePanel.SetActive(false);
+
+    }
+
+    public void CloseControls()
+    {
+        ControlsPanel.SetActive(false);
+    }
+
+    public void OpenControlsPanel()
+    {
+        ControlsPanel.SetActive(true);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        PausePanel.SetActive(true);
+        eventSystem.SetSelectedGameObject(currentSelectbutton);
     }
 }
